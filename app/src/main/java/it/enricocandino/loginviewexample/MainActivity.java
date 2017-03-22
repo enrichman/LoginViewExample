2fad521f580515df31cfcdf9cdd2c42e04df2722
Enrico Candino <enrico.candino@gmail.com>
2017-03-23 00:40:22 +0100

    initial commit

---
+++ app/src/main/java/it/enricocandino/loginviewexample/MainActivity.java
@@ -0,0 +1,45 @@
+package it.enricocandino.loginviewexample;
+
+import android.content.Context;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Context mContext = MainActivity.this;
+
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+    private Button loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        usernameEditText = (EditText) findViewById(R.id.username_edittext);
+        passwordEditText = (EditText) findViewById(R.id.password_edittext);
+        loginButton = (Button) findViewById(R.id.login_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = usernameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+                if (!username.equals("") && !password.equals("")) {
+                    Toast.makeText(mContext, "Logging in", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(mContext, "Missing username or password", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
